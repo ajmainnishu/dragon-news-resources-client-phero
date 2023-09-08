3c059ff3d525ddaa2a5f5df66ced1aa70742313a
Ajmain Nishu <ajmainnishu@gmail.com>
2023-09-08 09:25:18 +0600

    feat: SportsNews component added

---
+++ src/Components/LeftSide/LeftSide.jsx
@@ -0,0 +1,12 @@
+import SportsNews from "../SportsNews/SportsNews";
+
+
+const LeftSide = () => {
+    return (
+        <div>
+            <SportsNews></SportsNews>
+        </div>
+    );
+};
+
+export default LeftSide;
--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import RightSide from "../Components/RightSide/RightSide";
+import LeftSide from "../Components/LeftSide/LeftSide";
 
 const Home = () => {
     return (
@@ -10,7 +11,7 @@ const Home = () => {
             <Header></Header>
             <Container>
                 <Row>
-                    <Col xs={3}></Col>
+                    <Col xs={3}><LeftSide></LeftSide></Col>
                     <Col xs={6}></Col>
                     <Col xs={3}><RightSide></RightSide></Col>
                 </Row>
